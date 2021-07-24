d51a82f4068ba24478305eeb7dded3786cc72dc3
willtucker42 <willtucker42@gmail.com>
2021-07-24 14:10:50 -0700

    Added list adapter

---
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksAdapter.kt
@@ -0,0 +1,44 @@
+package com.codinginflow.mvvmtodo.ui.tasks
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.codinginflow.mvvmtodo.data.Task
+import com.codinginflow.mvvmtodo.databinding.ItemTaskBinding
+
+class TasksAdapter : ListAdapter<Task, TasksAdapter.TasksViewHolder>(DiffCallback()){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TasksViewHolder {
+        val binding = ItemTaskBinding.inflate(LayoutInflater.from(parent.context), parent,false)
+        return TasksViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TasksViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        holder.bind(currentItem)
+    }
+
+    class TasksViewHolder(private val binding: ItemTaskBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(task: Task){
+            binding.apply {
+                checkBoxCompleted.isChecked = task.completed
+                textViewTask.text = task.name
+                textViewTask.paint.isStrikeThruText = task.completed
+                labelPriority.isVisible = task.important
+            }
+        }
+    }
+    class DiffCallback : DiffUtil.ItemCallback<Task>(){
+        override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+}
